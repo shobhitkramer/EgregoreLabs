af61d149fa7127c6ea7ed2e288b4b20f9252afa5
shobhitkramer <shobhit.messi@gmail.com>
2020-05-25 10:41:37 +0530

    1st Commit

---
+++ test.R
@@ -0,0 +1,4 @@
+#This is test file to check stage,commit and push to EgregoreLabs repo on GitHub
+name = readline(prompt="Input your name: ")
+age =  readline(prompt="Input your age: ")
+print(paste("My name is",name, "and I am",age ,"years old."))
